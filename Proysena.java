38b41a59a16850142b965947ab19fa56f818ff00
LeonardoGomezRios <a.leonardo3460@gmail.com>
2025-04-30 11:26:03 -0500

    Update Proysena.java
    Añadimos las senticias faltantes para actualizacion, insercion y delete

--- Proysena.java
+++ Proysena.java
@@ -3,56 +3,109 @@
  * @author Leo G
  */
 
-package proysena;
+package porysena;
+
 
 import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class Proysena {
-
+public class Porysena {
 
     public static void main(String[] args) {
-        
-        //conexion a la base de datos
-        String usuario="root";
-        String password="Atento12+++";
-        String url="jdbc:mysql://localhost:3306/prueba"; 
-        
-        // creacion de los objetos
-        
-        Connection conexion;
-        Statement st;
-        ResultSet rs;
-        
-        
-        
+         
+    // Delcaracion de las variables para la base de datos 
+    
+    String usuario="root";
+    String password="Atento12+++";
+    String url="jdbc:mysql://localhost:3306/prueba";
+    
+    // Declaracion de los objetos para la base de datos
+    
+    Connection conexion;
+    Statement st;
+    ResultSet rs;
+    
+    // Establecemos el Class.forName con el try-catch
+    
         try {
-            // Inicializar
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException ex) {
-            Logger.getLogger(Proysena.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(Porysena.class.getName()).log(Level.SEVERE, null, ex);
         }
         
-            // Consult
+       // Establecemos conexion con getConnetion
         
         try {
-            conexion=DriverManager.getConnection(url, usuario, password);
+            conexion=DriverManager.getConnection(url,usuario,password);
             
             st=conexion.createStatement();
-            rs=st.executeQuery("SELECT * FROM usuarios");
+            
+            // CONSULTA usuarios
+            /*
+            rs=st.executeQuery("SELECT * FROM usuario");
             rs.next();
-            do{
-            System.out.println(rs.getInt("id")+ " : "+ rs.getString("nomusu"));
-                    }while(rs.next());
+             
+            do {
+                System.out.println(rs.getInt("usuarioID")+ " : "+ rs.getString("nombreUsuario")+ " : "+ rs.getString("correoUsuario") );            
+                
+            } while (rs.next());  
+             */
+
+            
+            //Sentencia INSERT para ingresar datos 
            
+            
+            /*
+            st.executeUpdate("INSERT INTO USUARIO VALUES(4, "
+                    + "'Maicol','asdf123','maicol@gmail.com',"
+                    + "'84728712','calle 654')");
+            rs=st.executeQuery("SELECT * FROM usuario");
+            rs.next();
+            
+            
+            do {
+                System.out.println(rs.getInt("usuarioID")+ 
+                        " : "+ rs.getString("nombreUsuario")+ 
+                        " : "+ rs.getString("correoUsuario") );            
+                
+            } while (rs.next()); */
+           
+           
+            //Sentencia UPDATE para actualizar datos 
+           
+            /*
+            st.executeUpdate("UPDATE usuario SET nombreUsuario= 'jorge' WHERE usuarioID=4");
+            rs=st.executeQuery("SELECT * FROM usuario");
+            rs.next();
+            
+            
+            do {
+                System.out.println(rs.getInt("usuarioID")+ 
+                        " : "+ rs.getString("nombreUsuario")+ 
+                        " : "+ rs.getString("correoUsuario") );            
+                
+            } while (rs.next());*/
+
+            // sentencia DELETE
+            
+            st.executeUpdate("DELETE FROM USUARIO WHERE USUARIOID=4");
+            rs=st.executeQuery("SELECT * FROM usuario");
+            rs.next();
+            
+            
+            do {
+                System.out.println(rs.getInt("usuarioID")+ 
+                        " : "+ rs.getString("nombreUsuario")+ 
+                        " : "+ rs.getString("correoUsuario") );            
+                
+            } while (rs.next());
+            
         } catch (SQLException ex) {
-            Logger.getLogger(Proysena.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(Porysena.class.getName()).log(Level.SEVERE, null, ex);
         }
         
-        
-         // INSERT
-        
+            
         
         
     }
